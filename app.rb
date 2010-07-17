0977c945afc0b7e9b3e473b66ec68f04c7fb0ed0
Emerson Vinicius <talk@evinicius.com>
2010-07-17 02:03:08 -0300

    Adicionando o querido Sinatra

--- app.rb
+++ app.rb
@@ -11,3 +11,22 @@ end
 
 get "/" do
 end
+
+get '/' do
+  haml :index
+end
+
+get '/scrap' do
+  
+end
+
+helpers do    
+  def exist?(options = {})
+    @data = ProjectOfLaw.first(:id => options[:id]) 
+    if @data
+      return true
+    else
+      return false
+    end
+  end
+end
