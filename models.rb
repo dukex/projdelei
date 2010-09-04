24826a7c76e5ff1f5b24c3d575d29bfa20669f61
Emerson Vinicius <talk@evinicius.com>
2010-09-04 14:21:29 -0300

    Versao 2.0 subindo

--- models.rb
+++ models.rb
@@ -1,8 +1,17 @@
-MongoMapper.connection = Mongo::Connection.new(ENV['MONGOHQ_URL'])
+require 'sinatra/mongomapper'
+
+
+if ENV['MONGOHQ_URL']
+  set :mongomapper, 'mongomapper://localhost:27017/example'
+else
+  set :mongomapper, 'mongomapper://localhost:27017/projdelei'
+end
+
+
 
 class ProjectOfLaw
   include MongoMapper::Document
   key :sileg, Integer
-  key :emenda, String
+  key :tweet, String
 end
 
