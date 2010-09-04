17b5f3769b7b0ea1e55a6a6f73608afaf831b687
Emerson Vinicius <talk@evinicius.com>
2010-09-04 15:11:48 -0300

    Concertando dependencias

--- models.rb
+++ models.rb
@@ -2,7 +2,8 @@ require 'sinatra/mongomapper'
 
 
 if ENV['MONGOHQ_URL']
-  set :mongomapper, 'mongomapper://localhost:27017/example'
+  database = ENV['MONGOHQ_URL'].gsub(/'mongodb'/,'mongomapper')
+  set :mongomapper, database
 else
   set :mongomapper, 'mongomapper://localhost:27017/projdelei'
 end
