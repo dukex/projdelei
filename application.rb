83bbdbcd64f56843ba51621336a300c276c1cdbe
Emerson Vinicius <talk@evinicius.com>
2010-09-27 21:26:47 -0300

    Add folder public

--- application.rb
+++ application.rb
@@ -4,6 +4,7 @@ require 'erb'
 
 set :root, File.dirname(__FILE__)
 set :views, Proc.new { File.join(root, "views") }
+set :public, Proc.new { File.join(root, "public") }
 
 not_found do
   erb :not_found
