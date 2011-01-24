25a60f1acaf47366ff34d64734d1b122c8e43d12
Emerson Vinicius <talk@evinicius.com>
2011-01-23 23:49:19 -0200

    added spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,12 @@
-require File.join(File.dirname(__FILE__), '..', 'application.rb')
+lib_dir = File.expand_path('../lib', __FILE__)
+$:.unshift lib_dir unless $:.include?(lib_dir)
 
-require 'rubygems'
+require 'rspec'
 require 'sinatra'
 require 'rack/test'
-require 'spec'
-require 'spec/autorun'
-require 'spec/interop/test'
+require 'app'
 
-# set test environment
 set :environment, :test
 set :run, false
 set :raise_errors, true
 set :logging, false
-
