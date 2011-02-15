e981ca803884d6bd201f678d88f7c309e6236e93
Emerson Vinicius <talk@evinicius.com>
2011-02-15 03:03:04 -0200

    updated spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
+require 'bundler'
+Bundler.require
+
 lib_dir = File.expand_path('../lib', __FILE__)
 $:.unshift lib_dir unless $:.include?(lib_dir)
-require 'bundler'
-
-Bundler.require
 
 require 'rspec'
 require 'sinatra'
