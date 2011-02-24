b968f0e96812c5d057d6a3c3fc463656dc35d2ec
Emerson Vinicius <talk@evinicius.com>
2011-02-24 03:55:54 -0300

    updated factory

--- spec/support/factories/LawProject.rb
+++ spec/support/factories/LawProject.rb
@@ -3,6 +3,6 @@ Factory.define :law_project do |f|
   f.pl_id 000
   f.proposition "PL-2001/1000"
   f.link "http://google.com"
-  f.explication "Lorem "
+  f.explication "Lorem ipsum dolor sit amet, consectetur magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
 end
 
