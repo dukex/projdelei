366d1b53bf6b2361a2a8496cecc2ef35ad5f4df2
Emerson Vinicius <talk@evinicius.com>
2011-02-24 04:05:49 -0300

    added twitter update in cron

--- cron.sh
+++ cron.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /var/local/apps/projdelei && bundle exec rake scraper:run
+cd /var/local/apps/projdelei && bundle exec rake scraper:run && cd /var/local/apps/projdelei && rake twitter:update
