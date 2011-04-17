ca521197e28cb3da3576422f91be5e1722b98adc
Emerson Vinicius <talk@evinicius.com>
2011-04-17 00:09:55 -0300

    fix cron.sh, rake not need bundle exec

--- cron.sh
+++ cron.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /var/local/apps/projdelei && bundle exec rake scraper:run && cd /var/local/apps/projdelei && rake twitter:update
+cd /var/local/apps/projdelei && rake scraper:run && cd /var/local/apps/projdelei && rake twitter:update
