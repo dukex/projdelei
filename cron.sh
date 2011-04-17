b1df465c40acca9850fd7b10158fe21ad54bbb66
Emerson Vinicius <talk@evinicius.com>
2011-04-17 18:14:10 -0300

    added logs

--- cron.sh
+++ cron.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-cd /var/local/apps/projdelei && rake scraper:run && cd /var/local/apps/projdelei && rake twitter:update
+cd /var/local/apps/projdelei
+rake scraper:run >> logs/scraper.log
+rake twitter:update >> logs/updater.log
