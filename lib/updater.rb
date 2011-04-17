d68d255e67f4b09b64925aa834614dc8eeaeeb7e
Emerson Vinicius <talk@evinicius.com>
2011-04-17 02:03:29 -0300

    fix updater

--- lib/updater.rb
+++ lib/updater.rb
@@ -1,7 +1,8 @@
 class Updater
   def self.now
-    law = LawProject.first(:was_shared => false)
-    Twitter.update(law.tweet)
-    law.update(:was_shared => true)
+     law = LawProject.first(:was_shared => false)
+     Twitter.update("#{law.tweet.clean}", :include_entities => true)
+     law.update(:was_shared => true)
   end
 end
+
