129e1ecf5f4b5a17e7f341b6eee05a880660f84d
Emerson Vinicius <talk@evinicius.com>
2011-04-17 21:46:33 -0300

    update deploy.sh, added whenever command

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git push origin master
-ssh -A deploy@67.23.242.87 'cd /var/local/apps/projdelei && git pull origin master && bundle install --path ~/.bundle --without development test cucumber environment=production'
+ssh -A deploy@67.23.242.87 'cd /var/local/apps/projdelei && git pull origin master && bundle install --path ~/.bundle --without development test cucumber environment=production && whenever -w'
