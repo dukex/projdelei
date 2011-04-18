91a4884536f1ce16b8f1d54f17975bb966185d1a
Emerson Vinicius <talk@evinicius.com>
2011-04-18 00:59:12 -0300

    remove ' of schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,6 @@ set :output, "/var/local/apps/projdelei/logs/cron.log"
 
 every 1.hour do
   %w{scraper:run twitter:update}.each do |task|
-    command "source ~/.env && cd /var/local/apps/projdelei && bundle exec rake #{task} --silent environment=production'"
+    command "source ~/.env && cd /var/local/apps/projdelei && bundle exec rake #{task} --silent environment=production"
   end
 end
