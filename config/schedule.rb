319a22db69dd9939bf56b4b065692fa1ccfda7f3
Emerson Vinicius <talk@evinicius.com>
2011-04-18 21:03:28 -0300

    changed update from 1 hours to 30 minutes

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 set :output, "/var/local/apps/projdelei/logs/cron.log"
 
-every 1.hour do
+every 30.minutes do
   %w{scraper:run twitter:update}.each do |task|
     command "source ~/.env && cd /var/local/apps/projdelei && bundle exec rake #{task} --silent environment=production"
   end
