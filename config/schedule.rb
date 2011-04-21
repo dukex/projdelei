8025ebae5d462de455bd85953d68fa23f32e182f
Emerson Vinicius <talk@evinicius.com>
2011-04-21 19:58:46 -0300

    update schedule for better real time

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 set :output, "/var/local/apps/projdelei/logs/cron.log"
 
-every 30.minutes do
+every 20.minutes do
   %w{scraper:run twitter:update}.each do |task|
     command "source ~/.env && cd /var/local/apps/projdelei && bundle exec rake #{task} --silent environment=production"
   end
