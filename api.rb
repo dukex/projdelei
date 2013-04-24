cb51eab121a8c2a45e7f815f91fd0088da463e23
Duke <duke@vertigem.xxx>
2013-04-24 13:03:49 -0300

    require models and grape hack

--- api.rb
+++ api.rb
@@ -1,6 +1,12 @@
 require 'bundler'
 Bundler.require
 
+lib_dir = File.expand_path('../lib', __FILE__)
+$:.unshift lib_dir unless $:.include?(lib_dir)
+
+require 'models'
+require 'grape_hack'
+
 module ProjDeLei
   class API < Grape::API
   end
