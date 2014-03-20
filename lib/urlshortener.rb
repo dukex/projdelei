8683ee9575e43b663d47d249e98c38cfce4be5c1
Duke <duke@riseup.net>
2014-03-20 14:20:07 -0300

    fixed shortener

--- lib/urlshortener.rb
+++ lib/urlshortener.rb
@@ -2,6 +2,6 @@ require 'open-uri'
 
 class UrlShortener
   def self.shorten(url)
- open("http://is.gd/create.php?format=simple&longUrl=#{url}&format=txt").read
+    open("http://is.gd/create.php?format=simple&url=#{url}").read
   end
 end
