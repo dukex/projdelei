1e0657059c27f659dc0b4633dd10d6efe215c8d3
Duke <duke@riseup.net>
2014-03-20 14:04:36 -0300

    added log

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,6 +7,7 @@ class Scraper
   def self.run!
     LawProjectParser.each do |project_law|
       if LawProject.all(:pl_id => project_law.pl_id).count == 0
+        puts "Creating #{project_law.proposition}..."
         LawProject.create! :pl_id => project_law.pl_id,
                     :proposition => project_law.proposition,
                     :link => project_law.link,
