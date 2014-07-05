0d98e50f07cfafcdb1e4e0d9e66f1428f9079691
Duke <duke@riseup.net>
2014-07-05 20:19:05 -0300

    removed facebook update by Updater

--- lib/updater.rb
+++ lib/updater.rb
@@ -6,7 +6,6 @@ class Updater
     law = LawProject.first(was_shared: false)
     if(law)
       Twitter.update("#{ic.iconv(law.tweet.clean)}", include_entities: true)
-      FACEBOOK.put_connections(FACEBOOK_PAGE_ID, "feed", message: "#{ic.iconv(law.facebook_status)}")
       law.update(was_shared: true)
     end
   end
